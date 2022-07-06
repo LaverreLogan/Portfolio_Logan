0eb4787a8f209e8692bcb9b247745736ab8d262b
LaverreLogan <91594847+LaverreLogan@users.noreply.github.com>
2022-07-06 20:49:00 +0200

    Up mainpage

--- styles/main.js
+++ styles/main.js
@@ -1,27 +1,25 @@
 
 const title = document.getElementById('mainTitle');
-const text = "Hi, I am Logan Web Developper";
+const text = "Hi, I am Logan, Web Developper";
 let index = 0;
 const randomSpeed = (min , max) => {
     return Math.floor(Math.random() * (max - min) + min)
 };
 
-var caret = "<span style='color:#60B450'> _</span>";
+const caret = "<span style='color:#60B450'> _</span>";
 const play = () => {
     if (index > text.length) {
-        console.log('end');
         index = 0;
         timer = setTimeout(play, 5000);
     }else{
-        caret === "<span style='color:#60B450'>_</span>" ? caret = "" : caret = "<span style='color:#60B450'>_</span>";
+        caret === "<span style='color:#60B450'>|</span>" ? caret = "" : caret = "<span style='color:#60B450'>|</span>";
 
         if (index > text.length-1) {
             caret = "";
         }
-        console.log(index)
         title.innerHTML = text.slice(0, index) + caret;
         index++;
         timer = setTimeout(play, randomSpeed(50, 300));
     }   
 };
-var timer = setTimeout(play);
+let timer = setTimeout(play);
