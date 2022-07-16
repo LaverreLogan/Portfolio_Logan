39f98dc745815f3fab6fa36fe3acfef3bf72390d
LaverreLogan <91594847+LaverreLogan@users.noreply.github.com>
2022-07-16 15:06:33 +0200

    Up Topbar

---
+++ script/main.js
@@ -0,0 +1,18 @@
+const sections = document.querySelectorAll('section');
+const navList = document.querySelectorAll('nav .menu ul li');
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach( section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (scrollY >= (sectionTop - sectionHeight / 3)) {
+            current = section.getAttribute('id');
+        }
+    })
+    navList.forEach(li => {
+        li.classList.remove('active');
+        if (li.classList.contains(current)) {
+            li.classList.add('active')
+        }
+    })
+})
