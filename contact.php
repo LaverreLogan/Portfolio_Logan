a42bc7153cbe079e239453a0b8142b7741577a60
LaverreLogan <91594847+LaverreLogan@users.noreply.github.com>
2022-08-27 03:38:26 +0200

    up

--- contact.php
+++ contact.php
@@ -11,7 +11,6 @@
 		'email' => 'E-mail',
 		'message' => 'Message',
 	];
-
 	define('SITE_KEY', '6Lfb4KQhAAAAAPi5jACDfRncMywjZHqIqnEzJLQD');
 	define('SECRET_KEY', '6Lfb4KQhAAAAACIo7h7JDfgO4PtrxEgoxt8iifPP');
 	/**
@@ -19,12 +18,11 @@
 	 */
 
 	function isFormSubmited($keys) {
-		foreach ($keys as $key) {
-			if (empty($_POST[$key])) {
-				header('Location: /#page5');
-				return false;
-			}
-		}
+		// foreach ($keys as $key) {
+			// if (empty($_POST[$key])) {
+			// 	return false;
+			// }
+		// }
 		return true;
 	}
 
@@ -38,16 +36,14 @@
 		return $data;
 	}
 
-	function sendEmail($to_email, $data) {
-		$content = "Bonjour,\r\ntu as reçu un nouveau contact depuis le site laverre-logan.com.\r\n";
+	function sendEmail($email, $data) {
+		$content = "Bonjour,\r\nTu as reçu un nouveau message depuis le site laverre-logan.com.\r\n";
 
 		foreach ($data as $label => $value) {
-			$content .= $label . "\r\n";
-			$content .= wordwrap($value, 70, "\r\n");
-			$content .= "\r\n";
+			$content .= $label .":". wordwrap($value, 70). "\r\n";
 		}
 
-		return mail($to_email, 'Nouveau contact', $content);
+		return mail($email, 'Nouveau contact', $content);
 	}
 
 	function saveContact($data) {
@@ -72,17 +68,29 @@
 			sendEmail($email, $contact);
 		} catch (\Exception $e) 
 		{ unset($e); }
-		
+
+
 
 		header('Location: /#page5');
 		exit;
 	}
 
-	if($_POST) {
-		function getCaptcha($secretKey) {
-			$response = file_get_contents("https://www.google.com.recaptcha/api/siteverify?secret=".SECRET_KEY."&response={$secretKey}");
-			$jsonResponse = json_decode($response);
-			return $jsonResponse;
+	if(isset($_POST['g-recaptcha-response'])) {
+		$captcha = $_POST['g-recaptca-response'];
+	} else {
+		$captcha = false;
+	}
+
+	if (!$captcha) {
+		echo "<script>alert \"Error in captcha\"</script>";
+	} else {
+		$secret = "6Lfb4KQhAAAAACIo7h7JDfgO4PtrxEgoxt8iifPP";
+		$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=" . $secret . "&response=" . $captcha . "&remoteip=" . $_SERVER['REMOTE_ADDR']);
+		$response = json_decode($response);
+		if ($response->succes === false) {
+			echo "<script>alert \"Error in response\"</script>";
 		}
-		$captcha = getCaptcha($_Post['g-recaptcha-response']);
+	}
+	if ($response->success==true && $response->score <= 0.5) {
+		echo "<script>alert \"Robot\"</script>";
 	}
